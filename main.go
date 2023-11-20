ca34f70dce497619d0a3b64e8e6c96dc0d41c88c
Cameron <freeman64.CH@googlemail.com>
2023-11-20 20:17:35 +0000

    Update main.go
    In line with renaming

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-    "github.com/nrexception/tf-provider-concept/pkg/provider"
+    "github.com/nrexception/b2cterraformextensions/pkg/provider"
     "github.com/hashicorp/terraform-plugin-sdk/plugin"
     "github.com/hashicorp/terraform-plugin-sdk/terraform"
 )
