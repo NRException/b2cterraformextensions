0ad6b9066eed7c133231513b69fa2be41975683d
Cameron Huggett <cameron.huggett@zonal.co.uk>
2023-11-20 15:32:32 +0000

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/nrexception/tf-provider-concept
+
+go 1.21.4
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+
+    "fmt"
+
+    "github.com/nrexception/tf-provider-demo/pkg/provider"
+
+)
